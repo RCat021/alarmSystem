1e740af0dd157f8c233abcc0ab12a5c75d282c4f
RCat021 <rinatsarafeev65@gmail.com>
2025-05-27 09:12:37 +0500

    упрощение корутины

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,11 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Alarm : MonoBehaviour
 {
     [SerializeField] private AudioSource _alarmSystem;
-    [SerializeField] private float _stepVolume = 0.1f;
+    [SerializeField] private float _speed = 0.1f;
     [SerializeField] private float _stepTime = 0.1f;
 
     private float _maxVolume = 1f;
@@ -30,28 +29,19 @@ public class Alarm : MonoBehaviour
 
     private void SetNewAlarm(float volume)
     {
-        if(_activeCoroutine != null) 
-            StopCoroutine( _activeCoroutine);
+        if (_activeCoroutine != null)
+            StopCoroutine(_activeCoroutine);
 
         _activeCoroutine = StartCoroutine(SetValue(volume));
     }
 
     private IEnumerator SetValue(float endVolume)
     {
-        float startVolume = _alarmSystem.volume;
-        float direction = Mathf.Sign(endVolume - startVolume); 
-        float nextVolume = startVolume;
-
-        var time = new WaitForSeconds(_stepTime);
-
-        while (direction > 0 ? nextVolume < endVolume : nextVolume > endVolume)
+        while (Mathf.Approximately(_alarmSystem.volume, endVolume) == false)
         {
-            nextVolume += _stepVolume * direction; 
-            _alarmSystem.volume = Mathf.Clamp(nextVolume, _minVolume, _maxVolume);
-            yield return time;
-        }
+            _alarmSystem.volume = Mathf.MoveTowards(_alarmSystem.volume, endVolume, _speed * Time.deltaTime);
 
-        _alarmSystem.volume = Mathf.Clamp(endVolume, _minVolume, _maxVolume);
-        _activeCoroutine = null;
+            yield return null;
+        }
     }
 }
